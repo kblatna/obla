5c8d559664894631d68f5d85e31f923a5e87c0a8
blatnak <katerina.blatna@email.cz>
2024-06-03 15:03:16 +0200

    vue, bootstrap, sass etc. install, settings

---
+++ obla-flat/src/main.ts
@@ -0,0 +1,7 @@
+import './assets/scss/app.scss' 
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
