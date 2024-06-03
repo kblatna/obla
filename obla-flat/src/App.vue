288d76b6f024f05777bffd12756f3cb1cb1cb456
blatnak <katerina.blatna@email.cz>
2024-06-03 15:44:21 +0200

    header & footer added

--- obla-flat/src/App.vue
+++ obla-flat/src/App.vue
@@ -1,4 +1,42 @@
 <template>
-  <h1>Oblá byt</h1>
-</template>
+    <header>
+        <BNavbar toggleable="lg" variant="dark" container="false">
+            <BNavbarBrand href="/">Oblá 36</BNavbarBrand>
+            <BNavbarToggle target="nav-collapse" />
+            <BCollapse id="nav-collapse" is-nav>
+                <BNavbarNav class="ms-auto mb-2 mb-lg-0">
+                    <BNavItem href="/" class="text-white-50">Info</BNavItem>
+                </BNavbarNav>
+            </BCollapse>
+        </BNavbar>
+    </header>
+    
+    <div class="container">
+        <h1>Oblá 36</h1>
+    </div>
 
+    <footer class="footer text-center text-lg-start text-white mt-5">
+        <section class="container pt-4">
+            <div class="text-center text-sm-start mb-3">
+                <div class="row">
+                    <div class="col-12 col-md-6 col-lg-3 mb-4">
+                        <h6 class="footer-col-title text-uppercase fw-bold">Kontakt na majitele:</h6>                    
+                        <div class="mt-5">
+                            Tomáš Cibulka
+                            Oblá 36<br>
+                            625 00 BRNO
+                        </div>
+                        <div>                            
+                            E-mail: <a href="mailto:t.cibulka87@gmail.com">t.cibulka87@gmail.com</a><br>
+                            Telefon: <a href="tel:+420725695145">725 695 145</a>
+                        </div>
+                    </div>                    
+                </div>
+            </div>
+        </section>
+
+        <div class="footer-bottom text-center p-3">
+            <span class="opacity-50">© 2024 Kateřina Blatná</span>
+        </div>
+    </footer>
+</template>
