e77d9193e43bec0c685d7daaf334678827342da6
blatnak <katerina.blatna@email.cz>
2024-06-14 13:21:20 +0200

    photogallery fix

--- src/components/PhotoGallery.vue
+++ src/components/PhotoGallery.vue
@@ -4,10 +4,11 @@
       <swiper
         :modules="[Thumbs]"
         @swiper="setThumbsSwiper"
+        class="main-photo"
       >
         <swiper-slide v-for="(photo, index) in photos" :key="index">
           <a :href="`/images/DSC_${photo.src}.jpg`" data-lg-size="1600-2400">
-            <img :alt="photo.alt" :src="`/images/DSC_${photo.src}.jpg`" class="rounded-2 w-100 h-100" />
+            <img :alt="photo.alt" :src="`/images/DSC_${photo.src}.jpg`" class="rounded-2 w-100" />
           </a>
         </swiper-slide>
       </swiper>
@@ -17,7 +18,7 @@
     </div>
   </div>
 
-  <div class="my-2">
+  <div class="my-2" v-lightgallery>
     <swiper
       :modules="modules"
       :slides-per-view="6"
@@ -25,11 +26,11 @@
       :loop="true"
       :navigation="true"
       :thumbs="{ swiper: thumbsSwiper }"
-      class="test"
+      class="thumbs"
     >
       <swiper-slide v-for="(photo, index) in photos" :key="index">
         <a :href="`/images/DSC_${photo.src}.jpg`" data-lg-size="1600-2400">
-          <img :alt="photo.alt" :src="`/images/DSC_${photo.src}.jpg`" class="rounded-2 w-100 h-100" />
+          <img :alt="photo.alt" :src="`/images/DSC_${photo.src}.jpg`" class="rounded-2" />
         </a>
       </swiper-slide>
     </swiper>  
@@ -84,18 +85,23 @@ const photos = ref([
 
 .swiper {
   width: 100%;
-  height: 100%;
+  height: 300px;
+  margin-left: auto;
+  margin-right: auto;
   --swiper-navigation-color: #fff;
   --swiper-theme-color: #fff;
 }
 
 .swiper-slide {
+  background-size: cover;
+  background-position: center;
   text-align: center;
-  font-size: 18px;
+  font-size: 16px;
   background: #fff;
   display: flex;
   justify-content: center;
   align-items: center;
+  height: auto;
 }
 
 .swiper-slide img {
@@ -105,155 +111,15 @@ const photos = ref([
   object-fit: cover;
 }
 
-.swiper {
-  width: 100%;  
-  margin-left: auto;
-  margin-right: auto;
+.main-photo img {
+  height: 18.8rem;
+  width: 18.8rem;
 }
 
-
-
-.swiper-slide {
-  background-size: cover;
-  background-position: center;
+.thumbs img {
+  height: 9.4rem;
+  width: 9.4rem;
 }
 
-.swiper-slide img {
-  display: block;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
 </style>
-
-
- <!-- <template>        
-  <div class="d-flex">
-    <div class="col-12 col-md-6">
-      <div v-lightgallery>
-        <swiper
-          :modules="[Thumbs]"
-          @swiper="setThumbsSwiper"
-          :slides-per-view="1"
-        >
-          <swiper-slide v-for="(photo, index) in photos" :key="index">
-            <a :href="`/images/DSC_${photo.src}.jpg`" data-lg-size="1600-2400">
-              <img :alt="photo.alt" :src="`/images/DSC_${photo.src}.jpg`" class="rounded-2 w-100 h-100" />
-            </a>
-          </swiper-slide>
-        </swiper>
-      </div>
-    </div>
-    <div class="col-12 col-md-6 p-3">
-      dfasdeflasfe
-    </div>
-  </div>
-
-  <div class="my-2">
-    <swiper
-      :modules="modules"
-      :slides-per-view="6"
-      :space-between="10"
-      :loop="true"
-      :navigation="true"
-      :thumbs="{ swiper: thumbsSwiper }"
-    >
-      <swiper-slide v-for="(photo, index) in photos" :key="index">
-        <a :href="`/images/DSC_${photo.src}.jpg`" data-lg-size="1600-2400">
-          <img :alt="photo.alt" :src="`/images/DSC_${photo.src}.jpg`" class="rounded-2 w-100 h-100" />
-        </a>
-      </swiper-slide>
-    </swiper>  
-  </div>
-</template>
-
-
-
-<script setup lang="ts">
-import { ref } from 'vue'
-import { Swiper, SwiperSlide } from 'swiper/vue'
-import { Navigation, Pagination, Scrollbar, Thumbs } from 'swiper/modules'
-import vLightgallery from './vLightgallery'
-
-// Import Swiper styles
-import 'swiper/scss'
-import 'swiper/scss/navigation'
-import 'swiper/scss/pagination'
-import 'swiper/scss/scrollbar'
-import 'swiper/scss/thumbs'
-
-// Import LightGallery styles
-import 'lightgallery/css/lightgallery.css'
-import 'lightgallery/css/lg-thumbnail.css'
-import 'lightgallery/css/lg-zoom.css'
-
-const thumbsSwiper = ref(null)
-
-const setThumbsSwiper = (swiper: any) => {
-  thumbsSwiper.value = swiper
-}
-
-const modules = [Navigation, Pagination, Scrollbar, Thumbs]
-
-// Define the photos array
-const photos = ref([
-  { src: '0018', alt: 'Obývací pokoj' },
-  { src: '0019', alt: 'Kuchyně' },
-  { src: '0020', alt: 'Ložnice' },
-  { src: '0032', alt: 'Koupelna' },
-  { src: '0033', alt: 'Chodba' },
-  { src: '0036', alt: 'blabla' },
-  { src: '0042', alt: 'Obývací pokoj' },
-  { src: '0047', alt: 'Kuchyně' },
-  { src: '0054', alt: 'Ložnice' },
-  { src: '0059', alt: 'Koupelna' },
-  { src: '0067', alt: 'Chodba' },
-  { src: '0079', alt: 'blabla' }
-])
-</script>
-
-<style>
-.swiper {
-  width: 100%;
-  height: 100%;
-}
-
-.swiper-slide {
-  text-align: center;
-  font-size: 18px;
-  background: #fff;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.swiper-slide img {
-  display: block;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
-
-.swiper {
-  width: 100%;  
-  margin-left: auto;
-  margin-right: auto;
-}
-
-.swiper-wrapper {
-  height: 150px;
-}
-
-.swiper-slide {
-  background-size: cover;
-  background-position: center;
-}
-
-.swiper-slide img {
-  display: block;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
-</style>
- -->
+ 
--- src/components/vLightgallery.ts
+++ src/components/vLightgallery.ts
@@ -31,37 +31,3 @@ const vLightgallery = {
 }
 
 export default vLightgallery
-
-// import { type DirectiveBinding } from 'vue'
-// import lightGallery from 'lightgallery'
-// import lgThumbnail from 'lightgallery/plugins/thumbnail'
-// import lgZoom from 'lightgallery/plugins/zoom'
-// import lgAutoplay from 'lightgallery/plugins/autoplay'
-// import lgFullscreen from 'lightgallery/plugins/fullscreen'
-
-// declare global {
-//   interface HTMLElement {
-//     lightGallery?: any;
-//   }
-// }
-
-// const vLightgallery = {
-//   mounted(el: HTMLElement, binding: DirectiveBinding) {
-//     el.lightGallery = lightGallery(el, {
-//       plugins: [lgThumbnail, lgZoom, lgAutoplay, lgFullscreen],
-//       speed: 500,
-//       mode: 'lg-slide',
-//       selector: 'a',
-//       controls: true, // Ensure navigation controls are enabled
-//       download: false, // Disable the download button if not needed
-//       ...binding.value
-//     })
-//   },
-//   unmounted(el: HTMLElement) {
-//     if (el.lightGallery) {
-//       el.lightGallery.destroy(true)
-//     }
-//   }
-// }
-
-// export default vLightgallery
