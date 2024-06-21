d51882dd3ed246ccfb666771f9fba79ed2f8d484
blatnak <katerina.blatna@email.cz>
2024-06-21 13:10:42 +0200

    iconify added to project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import './assets/scss/app.scss' 
 import '../node_modules/swiper/swiper.min.css'
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+import { Icon } from '@iconify/vue'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.component('IconifyIcon', Icon)
+
+app.mount('#app')
