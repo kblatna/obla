b0e1a10e447b8c875ad2560e4a41b4ef51167066
blatnak <katerina.blatna@email.cz>
2024-10-24 12:27:23 +0200

    fix: footer styles fix

--- src/components/FooterInfo.vue
+++ src/components/FooterInfo.vue
@@ -3,22 +3,20 @@
         <div class="row justify-content-center justify-content-md-start">
             <div class="col-12 col-md-4 my-4">
                 <h6 class="text-uppercase fw-bold">Kontakt na majitele:</h6>                    
-                <div class="mt-2">
-                    Tomáš Cibulka<br>
-                    Oblá 36<br>
-                    634 00 BRNO
-                </div>
-                <div>                            
-                    E-mail: <a href="mailto:t.cibulka87@gmail.com">t.cibulka87@gmail.com</a><br>
-                    Telefon: <a href="tel:+420725695145">725 695 145</a>
-                </div>
+                <address class="d-flex flex-column mt-2">
+                    <span>Tomáš Cibulka</span>
+                    <span>Oblá 36</span>
+                    <span>634 00 BRNO</span>
+                    <a href="mailto:t.cibulka87@gmail.com">t.cibulka87@gmail.com</a>
+                    <a href="tel:+420725695145">725 695 145</a>
+                </address>
             </div>        
             <div class="col-12 col-md-4 my-4">
                 <h6 class="text-uppercase fw-bold">Dokumenty:</h6>
-                <div class="mt-2">                            
-                    <a href="/pdf/prukaz_energeticke_narocnosti.pdf" target="_blank">Energetický štítek domu</a><br>
-                    <a href="/pdf/list_vlastnika_obla.pdf" target="_blank">Evidenční list vlastníka</a><br>
-                    <a href="/pdf/vyuctovani_druzba.pdf" target="_blank">Vyúčtování za rok 2023</a><br>
+                <div class="d-flex flex-column mt-2">                            
+                    <a href="/pdf/prukaz_energeticke_narocnosti.pdf" target="_blank">Energetický štítek domu</a>
+                    <a href="/pdf/list_vlastnika_obla.pdf" target="_blank">Evidenční list vlastníka</a>
+                    <a href="/pdf/vyuctovani_druzba.pdf" target="_blank">Vyúčtování za rok 2023</a>
                     <a href="/pdf/rozuctovani_nakladu.pdf" target="_blank">Rozpis nákladů na vytápění a vodu</a>
                 </div>
             </div>               
